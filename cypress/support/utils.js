b4cc24487a25e2da113c149a10e8aaeb7b28eab5
William Chen <crazcoldking@163.com>
2025-07-31 12:34:19 +0800

    resolve sidebar overlay issue

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,10 +1,18 @@
 import { Layout } from '../selectors/common/layout';
 
 export const ensureSidebarItemVisible = (itemSelector, device) => {
-  cy.get('body').then($body => {
+  cy.get('body').then(() => {
     if (device.viewport.width <= 1024) {
       cy.get(Layout.sidebarMenuToggle).click();
     }
-    cy.get(itemSelector).should('be.visible');
+    cy.get(itemSelector).scrollIntoView().should('be.visible');
+  });
+};
+
+export const ensureSidebarToggleInActive = (device) => {
+  cy.get('body').then(() => {
+    if (device.viewport.width <= 1024) {
+      cy.get(Layout.sidebarMenuToggle).should('not.have.class', 'active');
+    }
   });
 };
