480c06a2d25e016905f9fe05254a2d0eed13b0b9
William Chen <crazcoldking@163.com>
2025-07-31 12:34:40 +0800

    clean code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,23 +20,21 @@ Cypress.Commands.add('clickWhenVisible', (selector) => {
   cy.get(selector).should('be.visible').click();
 });
 
+
 Cypress.Commands.add('scrollAndVerifyVisible', (testId) => {
   cy.get(`[data-testid="${testId}"]`)
     .scrollIntoView({ duration: 1000, easing: 'swing' })
     .should('be.visible');
 });
 
-// clean up existed routes and services
+
 Cypress.Commands.add('cleanUpRoutesAndServices', () => {
-  // clean up routes
   cy.request('GET', 'http://localhost:8001/default/routes')
     .then((response) => {
       response.body.data.forEach((route) => {
         cy.request('DELETE', `http://localhost:8001/routes/${route.id}`);
       });
     });
-
-  // clean up services
   cy.request('GET', 'http://localhost:8001/default/services')
     .then((response) => {
       response.body.data.forEach((service) => {
